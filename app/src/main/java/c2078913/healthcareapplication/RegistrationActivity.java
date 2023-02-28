bc9bf5a41042fb724587c3dfae6293803ed02857
chycoder <chichuks02@gmail.com>
2023-02-28 06:25:43 -0800

    Text Id edit

---
+++ app/src/main/java/c2078913/healthcareapplication/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package c2078913.healthcareapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import c2078913.healthcareapplicstion.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
